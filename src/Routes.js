521ee01dd8c790e900d4cde83f121f6412e8b5d7
Michael Davison <mdavison92@Hotmail.com>
2021-05-30 16:46:29 -0400

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import App from "./App";
 import ProductsPage from "./Pages/ProductsPage";
-import Profile from "./Profile";
+import HomePage from "./Pages/HomePage";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={App} />
+                <Route exact path="/" component={HomePage} />
                 <Route exact path="/productsPage" component={ProductsPage} />
             </Switch>
         </BrowserRouter>
