8d53bed76a820b54df61ae9d387b0090d0c72d32
Michael Davison <mdavison92@Hotmail.com>
2021-05-31 14:33:02 -0400

    Added sidebar, productHeader, filter functionality

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,10 +1,60 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
+import allProducts from "../Data/allProducts"
+import ProductItem from "./ProductItem";
+import categories from "../Data/categories";
 
 
 const ProductList = (props) => {
 
+    const [currentProductList, setCurrentProductList] = useState(allProducts);
+    const [currentCategory, setCurrentCategory] = useState("All Products");
+
+    const clickHandler = (categoryArg) => {
+
+
+      setCurrentCategory(categoryArg);
+      
+      if(categoryArg == "All Products")
+      {
+        
+        setCurrentProductList(allProducts);
+      }
+      else {
+        
+        setCurrentProductList(allProducts.filter(product=>product.category == categoryArg));  
+      }
+       
+    }
+
+   
     return (
-        <div></div>
+        <div id = "productListWrapper">
+        
+              <div id ="productSideBar">
+                <h4>Sort by</h4>
+                    <li onClick={e=>clickHandler("All Products")}>All Products</li>
+                    <li onClick={e=>clickHandler("Consoles")}>Consoles</li>
+                    <li onClick={e=>clickHandler("Games")}>Games</li>
+                    <li onClick={e=>clickHandler("Merch")}>Merchandise</li>
+                 </div>
+                 <h2 id = "productListCategoryHeader">{currentCategory}</h2>
+
+                <div id = "product-list">
+                
+                    {currentProductList.map((product ) => {
+
+                        return (
+                            <ProductItem 
+                            name = {product.name}
+                            price = {product.price}
+                            image = {product.prevImage}
+                            />
+
+                        );
+                    })}
+                
+            </div>
+       </div>
     );
 
 
