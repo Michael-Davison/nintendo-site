a547156442118752f72a1a865edd883d78d89a0f
Michael Davison <mdavison92@Hotmail.com>
2021-05-31 14:31:48 -0400

    Update allProducts.js

--- src/Data/allProducts.js
+++ src/Data/allProducts.js
@@ -1,10 +1,12 @@
 import consoles from "./consoles";
 import games from "./games";
+import merch from "./merch";
 
 
 const products = [
   ...consoles,
   ...games,
+  ...merch,
 ];
 
 export default products;
