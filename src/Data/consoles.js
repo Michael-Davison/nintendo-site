340f1bc80759400ee04e7dbe81a0494d58f91ce1
Michael Davison <mdavison92@Hotmail.com>
2021-05-31 14:31:42 -0400

    Adding data

--- src/Data/consoles.js
+++ src/Data/consoles.js
@@ -1,7 +1,13 @@
 import switchBlueRed from "../Resources/Products/Consoles/switchBlueRed.jpg";
 import prevSwitchBlueRed from "../Resources/Products/Consoles/prevSwitchBlueRed.jpg";
-import switchGrey from "../Resources/Products/Consoles/switchGray.jpg";
-import prevSwitchGrey from "../Resources/Products/Consoles/prevSwitchGray.jpg";
+import switchGray from "../Resources/Products/Consoles/switchGray.jpg";
+import prevSwitchGray from "../Resources/Products/Consoles/prevSwitchGray.jpg";
+import switchAnimalCrossing from "../Resources/Products/Consoles/prevSwitchAnimalCrossing.jpg";
+import prevSwitchAnimalCrossing from "../Resources/Products/Consoles/prevSwitchAnimalCrossing.jpg";
+import switchLiteBlue from "../Resources/Products/Consoles/switchLiteBlue.jpg";
+import prevSwitchLiteBlue from "../Resources/Products/Consoles/prevSwitchLiteBlue.jpg";
+import switchLiteTurquoise from "../Resources/Products/Consoles/switchLiteTurquoise.jpg";
+import prevSwitchLiteTurquoise from "../Resources/Products/Consoles/prevSwitchLiteTurquoise.jpg";
 
 
 const consoles = [
@@ -16,11 +22,41 @@ const consoles = [
     {
 
         category: "Consoles",
-        id: "switch-Grey",
+        id: "switch-gray",
         name: "Nintendo Switch - Gray + Gray Joy-Con",
         price: 299.99,
-        image: switchGrey,
-        prevImage: prevSwitchGrey,
+        image: switchGray,
+        prevImage: prevSwitchGray,
+
+    },
+    {
+
+        category: "Consoles",
+        id: "switch-animal-crossing",
+        name: "Nintendo Switch Animal Crossing: New Horizons Edition",
+        price: 299.99,
+        image: switchAnimalCrossing,
+        prevImage: prevSwitchAnimalCrossing,
+
+    },
+    {
+
+        category: "Consoles",
+        id: "switch-lite-blue",
+        name: "Nintendo Switch Lite - Blue",
+        price: 299.99,
+        image: switchLiteBlue,
+        prevImage: prevSwitchLiteBlue,
+
+    },
+    {
+
+        category: "Consoles",
+        id: "switch-lite-turquoise",
+        name: "Nintendo Switch Lite - Turquoise",
+        price: 299.99,
+        image: switchLiteTurquoise,
+        prevImage: prevSwitchLiteTurquoise,
 
     },
 ];
--- src/Data/games.js
+++ src/Data/games.js
@@ -2,6 +2,12 @@ import prevHyrule from "../Resources/Products/Games/prevHyrule.jpg";
 import hyrule from "../Resources/Products/Games/hyrule.jpg";
 import animalCrossing from "../Resources/Products/Games/animalCrossing.jpg";
 import prevAnimalCrossing from "../Resources/Products/Games/prevAnimalCrossing.jpg";
+import hades from "../Resources/Products/Games/hades.jpg";
+import prevHades from "../Resources/Products/Games/prevHades.jpg";
+import luigisMansion from "../Resources/Products/Games/luigisMansion.jpg";
+import prevLuigisMansion from "../Resources/Products/Games/prevLuigisMansion.jpg";
+import botw from "../Resources/Products/Games/botw.jpg";
+import prevBotw from "../Resources/Products/Games/prevBotw.jpg";
 
 
 
@@ -22,6 +28,36 @@ const games = [
         price: 59.99,
         image: animalCrossing,
         prevImage: prevAnimalCrossing,
+
+    },
+    {
+
+        category: "Games",
+        id: "game-hades",
+        name: "Hades",
+        price: 34.99,
+        image: hades,
+        prevImage: prevHades,
+
+    },
+    {
+
+        category: "Games",
+        id: "game-luigis-mansion-3",
+        name: "Luigi's Mansion 3",
+        price: 49.99,
+        image: luigisMansion,
+        prevImage: prevLuigisMansion,
+
+    },
+    {
+
+        category: "Games",
+        id: "game-botw",
+        name: "The Legend of Zelda: Breath of the Wild",
+        price: 59.99,
+        image: botw,
+        prevImage: prevBotw,
 
     },
 ];
