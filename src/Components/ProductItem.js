9f20d540e802bee1b4442321f680a2b65f02d629
Michael Davison <mdavison92@Hotmail.com>
2021-05-31 14:33:25 -0400

    Implemented product-card

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,10 +1,26 @@
 import React, {Component} from "react";
 
 
+
+
+
+
 const ProductItem = (props) => {
 
+    
+
+
+    /*
+    display product.name, product.price, product.image
+    */
     return (
-        <div></div>
+        <div id = "product-Card" key = {props.image}>
+            <img src = {props.image} alt = {props.name}></img>
+            <h4>{props.name}</h4>
+            <h4>{props.price}</h4>
+           
+
+        </div>
     );
 
 
