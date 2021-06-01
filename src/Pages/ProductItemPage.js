ed470f38d351f56e6c597a1aea16329996ba26cf
Michael Davison <mdavison92@Hotmail.com>
2021-06-01 14:52:48 -0400

    Implemented productItemPage

--- src/Pages/ProductItemPage.js
+++ src/Pages/ProductItemPage.js
@@ -1,10 +1,28 @@
 import React, {Component} from "react";
+import NavBar from "../Components/NavBar";
+import Logo from "../Components/Logo";
+import { useParams } from "react-router-dom";
+import allProducts from "../Data/allProducts";
+import ProductItem from "../Components/ProductItem";
 
 
 const ProductItemPage = (props) => {
 
+    const { productId } = useParams();
+    const product = allProducts.find((product) => product.id === productId);
+
     return (
-        <div>ProductItemPage
+        <div id = "productItemPage">
+            <Logo/>
+            <NavBar/>
+            <div id ="productItemDisplay">
+                <ProductItem
+                                    name = {product.name}
+                                    price = {product.price}
+                                    image = {product.image}
+                                    id = {product.id}
+                                    />
+            </div>
             
         </div>
     );
