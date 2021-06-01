7ddf860eefee50dc3b2ae4b175d1d5f18f274049
Michael Davison <mdavison92@Hotmail.com>
2021-06-01 14:54:28 -0400

    Wrapped product-card image in Link

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 
 
@@ -11,11 +12,15 @@ const ProductItem = (props) => {
 
 
     /*
-    display product.name, product.price, product.image
+
+
     */
     return (
-        <div id = "product-Card" key = {props.image}>
+        <div id="product-Cards" key = {props.image}>
+            <Link to={`/store/products/${props.id}`}>
             <img src = {props.image} alt = {props.name}></img>
+            </Link>
+            
             <h5>{props.name}</h5>
             <h5>${props.price}</h5>
             <button>Add to Cart</button>
