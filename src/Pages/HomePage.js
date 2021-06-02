c4eebf809f5f6416c2dac44c1438f3b137633398
Michael Davison <mdavison92@Hotmail.com>
2021-06-01 22:40:25 -0400

    Implement homePage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import Logo from "../Components/Logo";
 import NavBar from "../Components/NavBar";
+import switchImage from "../Resources/Misc/homeSwitchImage.png";
 
 
 const HomePage = (props) => {
@@ -9,6 +10,17 @@ const HomePage = (props) => {
         <div>
              <Logo/>
              <NavBar/>
+             <div id = "showcase">
+                <h1>Nintendo Switch</h1>
+                <h2>Play anywhere, anytime, with anyone.</h2>
+                    <div id="showNowBtn">
+                        <button id ="">SHOP NOW</button>
+                    </div>
+             </div>
+              <div id = "showcaseImage">
+                <img src = {switchImage}></img>
+              </div>
+              
         </div>
     );
 
