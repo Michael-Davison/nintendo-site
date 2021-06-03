ffa9f20c7d412b9e79a8252ffa57363cde44fae2
Michael Davison <mdavison92@Hotmail.com>
2021-06-03 16:56:24 -0400

    Added onclick to button

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -8,13 +8,7 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 const ProductItem = (props) => {
 
-    
 
-
-    /*
-
-
-    */
     return (
         <div id="product-Cards" key = {props.image}>
             <Link to={`/store/products/${props.id}`}>
@@ -23,7 +17,7 @@ const ProductItem = (props) => {
             
             <h5>{props.name}</h5>
             <h5>${props.price}</h5>
-            <button>Add to Cart</button>
+            <button onClick={props.addToCart}>Add to Cart</button>
            
 
         </div>
