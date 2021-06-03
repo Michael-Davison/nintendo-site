d6294582d2616d662d8f1572e24f7ccfd07183bb
Michael Davison <mdavison92@Hotmail.com>
2021-06-03 16:56:59 -0400

    Removed dart from nav (debugging)

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,22 @@
 import {Navbar, Nav, Form, Button, FormControl} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import cartLogo from "../Resources/Logos/cartLogo.png";
+import ShoppingCart from "../Components/ShoppingCart";
+import React, {Component, useState} from "react";
 
 function NavBar() {
+
+  const [showCart, setShowCart] = useState(false);
+
+  const clickHandle = () => {
+
+    setShowCart(!showCart);
+  }
+    // {showCart ? <ShoppingCart/> : null}
+   
   return (
 
-    //<img id = "cartLogo"src = {cartLogo}></img>
-    //<img className ="nav-logo" src = {cartLogo}></img>
+   
     
     <div id = "navbar">
     <Navbar>
@@ -14,17 +24,19 @@ function NavBar() {
       <Nav className="ml-auto">
         <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="/ProductsPage">Store</Nav.Link>
-        <Nav.Link href="#">Cart Logo</Nav.Link>
-        
+        <Nav.Link href="#"><img onClick={clickHandle} width="30" height="30" src = {cartLogo}></img></Nav.Link>
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="dark">Search</Button>
       </Form>
     </Navbar>
+   
+    
+  
  
   </div>
   );
-}
+};
 
 export default NavBar;
