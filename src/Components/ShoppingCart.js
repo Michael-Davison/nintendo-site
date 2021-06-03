6f21398ccd25fc17101f9c1f65b49cf27237a15c
Michael Davison <mdavison92@Hotmail.com>
2021-06-03 16:53:34 -0400

    Shopping Cart Implementation

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,10 +1,99 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
+import allProducts from "../Data/allProducts"
 
 
 const ShoppingCart = (props) => {
 
+   
+    
+    const [cartArray, setCartArray] = useState([]);
+    const [currentProduct, setCurrentProduct] = useState(5);
+   
+
+    
+
+    useEffect (() => {
+        //addToCart(currentProduct);
+       
+        let temp = currentProduct;
+        console.log("UseEffect()- ShoppingCart.js");
+        console.log (currentProduct);
+
+
+    }, [currentProduct]);
+    
+
+    const addToCart = () => {
+
+        
+        console.log("Add to cart() - shoppingCart.js");
+      
+        /*
+        if(cartArray.includes(productID))
+        {
+            //increment quantity of productId
+            cartArray.find(product => product.id == productiD)//returns element value
+        }
+        */
+
+    }
+    /*
+    array1.find(element => element > 10);
+
+    Hooks
+    - cartArray
+        - cartItem(productId (passed), productName (passed), productPrice (passed),, quantity++, prevImage(passed) totalProductCost)
+    - totalQuantity
+    - SubTotal
+
+    Functions
+    - incrementQuantity (find(productID))
+    - decrementQuantiy (find(productID))
+    - computeTotalSum (iterate through array, adding up totalProductCost)
+    - removeProduct(find(productID))
+
+
+    Buttons
+    - increment/decrement next to quanity
+    - remove
+    - CheckOut
+
+    flow
+- customer clicks add to cart (passes Product.ID)
+- onClick, calls shoppingCart.addToCart(productID
+- addToCart(productID)
+	- checks if product exists in shoppingCartArray
+	- if yes, add to quantity
+
+	- if no, create cartItem object and add to shoppingCartArray
+
+use Effect
+
+computerSum
+
+[totalQuantity]
+
+    */
+
+
     return (
-        <div></div>
+        <div id="shoppingCartContainer">
+            <div id = "shoppingCart">
+                <div id = "shoppingCartHeader">
+                <h4>Shopping Cart</h4>
+                </div>
+                <ul>
+                <li></li>
+                <li>Item one </li>
+                <li>Item one </li>
+                <li>Item one </li>
+                <li>Item one </li>
+                </ul>
+            </div>
+           
+        </div>
+
+        
     );
 
 
--- src/Pages/ProductsPage.js
+++ src/Pages/ProductsPage.js
@@ -1,7 +1,8 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import ProductList from "../Components/ProductList"
 import Logo from "../Components/Logo";
 import NavBar from "../Components/NavBar";
+import ShoppingCart from "../Components/ShoppingCart";
 
 
 
@@ -9,11 +10,37 @@ import NavBar from "../Components/NavBar";
 
 const ProductsPage = (props) => {
 
+    const [productIdAdded, setProductIdAdded] = useState(0);
+    const [cartCount, setCartCount] = useState(0);
+
+    //when cartCount changes value, do this and render
+    useEffect (() =>
+    {       
+        console.log("ProductIdAdded: " + productIdAdded);
+            
+            console.log("UseEffect Called - Productpage.js");
+    }, [cartCount, productIdAdded]);
+
+    //Call back function when user selects add to cart
+    const handleCallback = (productId) => {
+        //user clicked add, change state
+        let tempCount = cartCount + 1;
+        setCartCount(tempCount);
+        let tempId = productId;
+        setProductIdAdded(productId);
+
+        console.log("HandleCallBack() - productsPage.js")
+
+        
+        
+       
+    }
     return (
         <div>
             <Logo/>
             <NavBar/>
-            <ProductList/>
+            <ShoppingCart productId = {productIdAdded}/>
+            <ProductList addCartHandler = {handleCallback}/>
 
 
         </div>
