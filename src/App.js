b454640f79a539cf4d21b8bfdcc3b1d1c93e4eea
Michael Davison <mdavison92@Hotmail.com>
2021-06-04 11:05:58 -0400

    Moved entry point to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import HomePage from "./Pages/HomePage.js";
+import ProductsPage from "./Pages/ProductsPage.js";
+import ProductItemPage from "./Pages/ProductItemPage.js";
 
 function App() {
   return (
     <div className="App">
-  
+      <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/productsPage" component={ProductsPage} />
+                <Route path="/store/products/:productId" component={ProductItemPage} />
+      </Switch>
     </div>
   );
 }
