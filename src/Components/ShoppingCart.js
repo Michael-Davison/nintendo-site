c0bda3a44b91a91b616ba14f2003cdecdd0dcf77
Michael Davison <mdavison92@Hotmail.com>
2021-06-06 21:54:05 -0400

    Debugging

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,100 +1,73 @@
 import React, {Component, useEffect, useState} from "react";
-import allProducts from "../Data/allProducts"
 
 
 const ShoppingCart = (props) => {
 
-   
-    
-    const [cartArray, setCartArray] = useState([]);
-    const [currentProduct, setCurrentProduct] = useState(5);
-   
+    var storedArray = localStorage.getItem('cartContents');
 
-    
-
-    useEffect (() => {
-        //addToCart(currentProduct);
-       
-        let temp = currentProduct;
-        console.log("UseEffect()- ShoppingCart.js");
-        console.log (currentProduct);
-
-
-    }, [currentProduct]);
-    
-
-    const addToCart = () => {
-
-        
-        console.log("Add to cart() - shoppingCart.js");
+    const testFunction = () => {
+        let temp = localStorage.getItem('cartContent');
       
-        /*
-        if(cartArray.includes(productID))
-        {
-            //increment quantity of productId
-            cartArray.find(product => product.id == productiD)//returns element value
-        }
-        */
+        console.log(temp);
 
+       localStorage.clear();
     }
+
     /*
-    array1.find(element => element > 10);
+         {currentProductList.map((product ) => {
+                       
+                        return (
+                            <ProductItem 
+                            name = {product.name}
+                            price = {product.price}
+                            image = {product.prevImage}
+                            id = {product.id}
+                            addToCart = {e=>props.addCartHandler(product.id)}
+                            />
 
-    Hooks
-    - cartArray
-        - cartItem(productId (passed), productName (passed), productPrice (passed),, quantity++, prevImage(passed) totalProductCost)
-    - totalQuantity
-    - SubTotal
-
-    Functions
-    - incrementQuantity (find(productID))
-    - decrementQuantiy (find(productID))
-    - computeTotalSum (iterate through array, adding up totalProductCost)
-    - removeProduct(find(productID))
-
-
-    Buttons
-    - increment/decrement next to quanity
-    - remove
-    - CheckOut
-
-    flow
-- customer clicks add to cart (passes Product.ID)
-- onClick, calls shoppingCart.addToCart(productID
-- addToCart(productID)
-	- checks if product exists in shoppingCartArray
-	- if yes, add to quantity
-
-	- if no, create cartItem object and add to shoppingCartArray
-
-use Effect
-
-computerSum
-
-[totalQuantity]
-
-    */
-
-
-    return (
+                        );
+                          return (
         <div id="shoppingCartContainer">
             <div id = "shoppingCart">
                 <div id = "shoppingCartHeader">
                 <h4>Shopping Cart</h4>
                 </div>
-                <ul>
-                <li></li>
-                <li>Item one </li>
-                <li>Item one </li>
-                <li>Item one </li>
-                <li>Item one </li>
-                </ul>
+                    <div id= "shoppingCartContents">
+                            <button onClick={testFunction}>test</button>
+                    </div>
+               
             </div>
            
         </div>
 
         
     );
+
+
+    */
+                          
+    return (
+        <div id="shoppingCartContainer">
+            <div id = "shoppingCart">
+                <div id = "shoppingCartHeader">
+                <h4>Shopping Cart</h4>
+                </div>
+                    <div id= "shoppingCartContents">
+                        {}
+                            {props.cartArray.map((product ) => {
+                                return(
+                                    <li>{product.name}</li>
+                                )
+                            })}
+                    </div>
+                    <button onClick={testFunction}>Check</button>
+            </div>
+            
+           
+        </div>
+    );
+   
+  
 
 
 };
