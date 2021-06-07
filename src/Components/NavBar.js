8264ad1f9d25172d85ee2f5f017c20857c50ae2d
Michael Davison <mdavison92@Hotmail.com>
2021-06-07 14:33:35 -0400

    Fixed bug causing cartArray state to lose Values.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,6 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import cartLogo from "../Resources/Logos/cartLogo.png";
 import ShoppingCart from "../Components/ShoppingCart";
 import React, {Component, useState} from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import HomePage from '../Pages/HomePage';
+import ProductsPage from '../Pages/ProductsPage';
 
 function NavBar() {
 
@@ -17,25 +25,24 @@ function NavBar() {
   return (
 
    
-    
     <div id = "navbar">
     <Navbar>
       <Navbar.Brand href="#home"></Navbar.Brand>
       <Nav className="ml-auto">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/ProductsPage">Store</Nav.Link>
-        <Nav.Link href="#"><img onClick={clickHandle} width="30" height="30" src = {cartLogo}></img></Nav.Link>
+        <Link to ="/">Home</Link>
+        <Link to ="/ProductsPage">Store</Link>
+        <Link to ="#"><img onClick={clickHandle} width="30" height="30" src = {cartLogo}></img></Link>
       </Nav>
       <Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
         <Button variant="dark">Search</Button>
       </Form>
     </Navbar>
-   
+    </div>
     
   
  
-  </div>
+  
   );
 };
 
