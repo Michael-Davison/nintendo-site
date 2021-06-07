615f25e4230eb144826b2851fafa796078bc0308
Michael Davison <mdavison92@Hotmail.com>
2021-06-06 21:53:34 -0400

    Implementing cart

--- src/App.js
+++ src/App.js
@@ -3,15 +3,61 @@ import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./Pages/HomePage.js";
 import ProductsPage from "./Pages/ProductsPage.js";
 import ProductItemPage from "./Pages/ProductItemPage.js";
+import React, {Component, useEffect, useState} from "react";
+import ShoppingCart from './Components/ShoppingCart.js';
+import allProducts from "./Data/allProducts";
 
-function App() {
+
+
+function App(props) {
+
+  const [cartArray, setCartArray] = useState([]);
+  const [currentProduct, setCurrentProduct] = useState();
+  const [showCart, setShowCart] = useState(true);
+
+ 
+
+ 
+  const addToCart = (productId) => {
+      //setTheArray(oldArray => [...oldArray, newElement]);
+      console.log(cartArray);
+      const product = allProducts.find((product) => product.id === productId);
+      setCartArray(cartArray => [...cartArray, product]);
+      localStorage.setItem('cartContent', JSON.stringify(cartArray));
+     
+    
+  
+}
+
+const toggleCartDisplay = () => {
+
+}
+
+const calculateCartTotal = () => {
+
+}
+
+//    {showCart ? <ShoppingCart cartArray = {cartArray}/> : null}
+
   return (
     <div className="App">
       <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/productsPage" component={ProductsPage} />
-                <Route path="/store/products/:productId" component={ProductItemPage} />
-      </Switch>
+                <Route exact path="/">
+                  <HomePage/>
+                </Route>
+                <Route exact path="/productsPage">
+                  <ProductsPage addToCart={addToCart}  />
+                </Route>
+                <Route path="/store/products/:productId">
+                  <ProductItemPage addToCart={addToCart} />
+                </Route> 
+               
+               
+       </Switch>
+       
+       <ShoppingCart cartArray = {cartArray}/>
+       
+       
     </div>
   );
 }
