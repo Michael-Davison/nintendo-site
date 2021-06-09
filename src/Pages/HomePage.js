9f920dce395152027229f8748a38935dc6263bfa
Michael Davison <mdavison92@Hotmail.com>
2021-06-08 23:07:38 -0400

    Update HomePage.js

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import switchImage from "../Resources/Misc/homeSwitchImage.png";
 
 
@@ -6,19 +6,19 @@ const HomePage = (props) => {
 
     return (
         <div>
-            
-             <div id = "showcase">
+
+            <div id="showcase">
                 <h1>Nintendo Switch</h1>
                 <h2>Play anywhere, anytime, with anyone.</h2>
-                    <div id="showNowBtn">
-                        <a id ="shopNowLink" href="/ProductsPage">SHOP NOW</a>
-                    </div>
-             </div>
-              <div id = "showcaseImage">
-                <img src = {switchImage}></img>
-              </div>
-              
+                <div id="showNowBtn">
+                    <a id="shopNowLink" href="/ProductsPage">SHOP NOW</a>
+                </div>
+            </div>
+            <div id="showcaseImage">
+                <img src={switchImage}></img>
+            </div>
         </div>
+
     );
 
 
