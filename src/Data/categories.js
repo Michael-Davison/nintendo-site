8e27e4c2f02af090026a15734a829cd461894363
Michael Davison <mdavison92@Hotmail.com>
2021-06-09 13:02:06 -0400

    Updates

--- src/Data/categories.js
+++
@@ -1,9 +0,0 @@
-const categories = [
-    {id: "consoles", name: "Consoles" },
-    {id: "games", name: "Games"},
-    {id: "merchandise", name: "Merchandise"},
- 
-  ];
-  
-  export default categories;
-  
